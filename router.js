64d856469a02e9a900c731e5f837e5a43be40cfd
Jauhar Basrai <jauhar.b@gmail.com>
2016-07-14 10:59:35 -0700

    Add analytics

--- router.js
+++ router.js
@@ -2,9 +2,11 @@ import express from 'express'
 import React from 'react'
 import ReactDOM from 'react-dom/server'
 import queryService from './queryService'
-import axios from 'axios'
+import fetch from 'isomorphic-fetch'
 
 const router = express.Router();
+const analytics = process.env.ANALYTICS + '/query'
+console.log(analytics)
 
 router.get('/query', (req, res) => {
     const query = req.query.q;
@@ -14,6 +16,21 @@ router.get('/query', (req, res) => {
         .search(query, articleType)
         .then(response => response.json())
         .then(data => res.json(data))
+
+    fetch(analytics, {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            term: query,
+            articleType: articleType,
+            ip: req.ip
+        })
+    }).then(() => {
+        console.log('sent to analytics')
+    }, err => console.log(err))
 });
 
 router.get(/^\/(search)?$/, (req, res) => {
--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import 'babel-polyfill'
-
 import Root from './Root'
 import React from 'react'
 import ReactDOM from 'react-dom'
